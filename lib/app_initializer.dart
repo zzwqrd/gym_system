29870e65ac59ee14fca24b9819c4596cff102a5f
zzwqrd <zeinelabdeen21@gmail.com>
2025-11-19 22:31:34 +0200

    add splash

--- lib/app_initializer.dart
+++ lib/app_initializer.dart
@@ -40,7 +40,7 @@ class AppInitializer {
       // ✅ 2. تهيئة القاعدة فعليًا (بعد الاسترجاع)
       final db = await DBHelper().database;
       print('✅ Database initialized successfully');
-      final data = await db.query('users');
+      final data = await db.query('products');
 
       if (data.isEmpty) {
         print(
